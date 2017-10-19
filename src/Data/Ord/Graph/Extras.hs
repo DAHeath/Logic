769e4c4602e1f76b1f3a673e9ceefe1a6eb50bdb
David Heath <heath.davidanthony@gmail.com>
2017-10-19 16:58:13 -0400

    more general unfold

--- src/Data/Ord/Graph/Extras.hs
+++ src/Data/Ord/Graph/Extras.hs
@@ -2,14 +2,35 @@
 module Data.Ord.Graph.Extras where
 
 import Control.Lens
+import Control.Monad.State
 import Data.Ord.Graph
 import Data.Maybe (fromJust)
+import qualified Data.Map as M
 import Data.Monoid ((<>))
 import qualified Turtle
 import Text.PrettyPrint.HughesPJClass (Pretty, prettyShow)
 
 backEdges :: Ord i => Graph i e v -> [((i, i), e)]
 backEdges g = filter (\((i1, i2), _) -> i2 <= i1) $ g ^@.. iallEdges
+
+unfold :: (Applicative f, Ord i)
+       => (i -> f i)
+       -> (i -> i -> e -> f e)
+       -> (i -> v -> f v)
+       -> i -> i -> e
+       -> Graph i e v -> f (Graph i e v)
+unfold fi fe fv i1 i2 e g =
+  let g' = reached i2 g
+      am = M.fromList <$> traverse (\i -> fmap (\i' -> (i, i')) (fi i)) (idxs g')
+      ae = fe i1 i2 e
+      ag = idfs fe fv g'
+  in connect <$> am <*> ae <*> pure g <*> (unfold' <$> am <*> ag)
+
+  where
+    unfold' m =
+      mapIdxs (\i -> M.findWithDefault i i m)
+    connect m e g g' =
+      addEdge i1 (M.findWithDefault i2 i2 m) e $ union g g'
 
 display :: (Eq i, Pretty i, Pretty e, Pretty v)
         => FilePath -> Graph i e v -> IO ()
--- src/Logic/ImplicationGraph/Type2.hs
+++ src/Logic/ImplicationGraph/Type2.hs
@@ -1,16 +1,83 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable, TypeFamilies, TemplateHaskell #-}
 module Logic.ImplicationGraph.Type2 where
 
-import Data.Data
+import           Control.Lens
+
+import           Data.Data
+import           Data.Ord.Graph (Graph)
+import qualified Data.Ord.Graph as G
+import qualified Data.Ord.Graph.Extras as G
+import           Data.Map (Map)
+import qualified Data.Map as M
+import           Data.List.Split (splitOn)
+
+import           Logic.Formula
+import           Logic.Var
+
+import           Text.PrettyPrint.HughesPJClass
+import           Text.Read (readMaybe)
+
+data Inst idx
+  = BaseInst [Var] Form
+  | AndInst [idx] (Inst idx)
+  | OrInst [[idx]] (Inst idx)
+  deriving (Show, Read, Eq, Ord, Data)
 
 data Vert idx
   = Instance (Inst idx)
   | Query Form
-  | Folded node
+  | Folded idx
+  deriving (Show, Read, Eq, Ord, Data)
+makePrisms ''Vert
+
+data Edge = Edge Form (Map Var Var)
   deriving (Show, Read, Eq, Ord, Data)
 
-data Inst idx
-  = Base [Var] Form
-  | And [idx] Inst
-  | Or [[idx]] Inst
+instance Pretty idx => Pretty (Vert idx) where
+  pPrint = \case
+    Instance i -> pPrint i
+    Query f -> pPrint f
+    Folded idx -> text "FOLD" <+> pPrint idx
+
+instance Pretty idx => Pretty (Inst idx) where
+  pPrint = \case
+    BaseInst vs f -> pPrint vs <+> pPrint f
+    AndInst idxs inner -> text "and" <+> pPrint idxs <+> pPrint inner
+    OrInst idxs inner -> text "or" <+> pPrint idxs <+> pPrint inner
+
+class (Show a, Ord a, Data a) => Idx a where
+  type BaseIdx a
+  baseIdx :: Lens' a (BaseIdx a)
+  toPrefix :: a -> String
+  fromPrefix :: String -> Maybe a
+  inst :: Lens' a Integer
+
+match :: (Idx a, Eq (BaseIdx a)) => a -> a -> Bool
+match x y = x ^. baseIdx == y ^. baseIdx
+
+data LinIdx = LinIdx { _linIden :: Integer, _linInst :: Integer }
   deriving (Show, Read, Eq, Ord, Data)
+makeLenses ''LinIdx
+
+instance Idx LinIdx where
+  type BaseIdx LinIdx = Integer
+  baseIdx = linIden
+  toPrefix (LinIdx iden i) = show iden ++ "_" ++ show i
+  fromPrefix s = case splitOn "_" s of
+      [iden, i] -> LinIdx <$> readMaybe iden <*> readMaybe i
+      _ -> Nothing
+  inst = linInst
+
+type ImplGr idx = Graph idx Edge (Vert idx)
+
+test = G.fromLists
+  [ (0, 'a')
+  , (1, 'b')
+  , (2, 'c')
+  , (3, 'd')
+  ]
+  [ (0, 1, 'w')
+  , (1, 2, 'x')
+  , (2, 3, 'y')
+  , (2, 4, 'z')
+  ]
