c000ad008ccb287102f50c05a1b0d8c4e5773256
Michael Eden <themichaeleden@gmail.com>
2017-10-26 08:19:06 -0400

    Added pretty print to implication graph.

--- java-parse/src/java/Ir.ml
+++ java-parse/src/java/Ir.ml
@@ -76,3 +76,50 @@ type command = Seq of command * command
              | Skip
 [@@deriving hash, compare, sexp]
 
+
+let rec pprint_kind = function
+  | Unit -> "V"
+  | Bool -> "B"
+  | Int -> "I"
+  | Real -> "F"
+  | List k -> Printf.sprintf "[%s]" (pprint_kind k)
+
+let pprint_var = function
+  | Bound (i, k) -> Printf.sprintf "(%d):%s" i (pprint_kind k)
+  | Free (n, k) -> Printf.sprintf "%s:%s" n (pprint_kind k)
+
+let rec pprint_binop op a b parens =
+  if parens
+  then Printf.sprintf "(%s %s %s)" (pprint_expr true a) op (pprint_expr true b)
+  else Printf.sprintf "%s %s %s" (pprint_expr true a) op (pprint_expr true b)
+
+and pprint_expr parens = function
+  (* Binops *)
+  | ExprCons (ExprCons (Distinct _, a), b) -> pprint_binop "!=" a b parens
+  | ExprCons (ExprCons (Eql _, a), b) -> pprint_binop "=" a b parens
+  | ExprCons (ExprCons (Lt _, a), b) -> pprint_binop "<" a b parens
+  | ExprCons (ExprCons (Le _, a), b) -> pprint_binop "<=" a b parens
+  | ExprCons (ExprCons (Gt  _, a), b) -> pprint_binop ">" a b parens
+  | ExprCons (ExprCons (Ge _, a), b) -> pprint_binop ">=" a b parens
+  | ExprCons (ExprCons (Add _, a), b) -> pprint_binop "+" a b parens
+  | ExprCons (ExprCons (Mul _, a), b) -> pprint_binop "*" a b parens
+  | ExprCons (ExprCons (Sub _, a), b) -> pprint_binop "-" a b parens
+  | ExprCons (ExprCons (Div _, a), b) -> pprint_binop "/" a b parens
+  | ExprCons (ExprCons (Mod _, a), b) -> pprint_binop "%" a b parens
+  | ExprCons (ExprCons (Impl, a), b) -> pprint_binop "->" a b parens
+  | ExprCons (ExprCons (Iff, a), b) -> pprint_binop "<->" a b parens
+  | ExprCons (ExprCons (And, a), b) -> pprint_binop "&&" a b parens
+  | ExprCons (ExprCons (Or, a), b) -> pprint_binop "||" a b parens
+
+  (* Uniops *)
+  | ExprCons (Not, a) -> Printf.sprintf "!%s" (pprint_expr true a)
+
+  (* Literals *)
+  | LUnit -> "()"
+  | LBool b -> Printf.sprintf "%B" b
+  | LInt i -> Printf.sprintf "%d" i
+  | LReal f -> Printf.sprintf "%f" f
+  | Var v -> pprint_var v
+
+  | other -> sexp_of_expr other |> Sexp.to_string
+
