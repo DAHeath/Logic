9532cd0a2c1e9529daa1432626e585fa602d66e3
Michael Eden <themichaeleden@gmail.com>
2017-11-08 17:45:23 -0500

    Added pruning algorithm to remove reducible vertices.

--- src/Logic/ImplicationGraph/Simplify.hs
+++ src/Logic/ImplicationGraph/Simplify.hs
@@ -1,9 +1,10 @@
 module Logic.ImplicationGraph.Simplify where
 
+import           Control.Lens
+
 import qualified Data.Map as Map
 import           Data.Maybe
 import qualified Data.Optic.Graph as G
-import qualified Data.Optic.Graph.Extras as G
 
 import qualified Logic.Var as V
 import           Logic.Formula
@@ -13,9 +14,9 @@ import           Logic.ImplicationGraph
 -- | Finds irreducible vertices in a given `ImplGr`.
 irreducible :: (Ord i) => ImplGr i -> [i]
 irreducible graph = [startIndex, queryIndex] ++ loopHeaders where
-  indices = G.idxs graph
-  startIndex = minimum indices
-  queryIndex = maximum indices
+  idxs = G.idxs graph
+  startIndex = minimum idxs
+  queryIndex = maximum idxs
 
   -- Find the loop headers, i.e. the destination vertices of back edges.
   loopHeaders = map (\((_, s), _) -> s) $ G.backEdges graph
@@ -46,3 +47,16 @@ conjunction edge edge' =
 disjunction :: Edge -> Edge -> Edge
 disjunction = undefined
 
+
+prune :: (Ord i) => ImplGr i -> ImplGr i
+prune graph = foldr removeVertex graph reducible where
+  andEdge (start, e) (end, e') =
+    G.addEdgeWith disjunction start end $ conjunction e e'
+
+  newEdges i g = cartesianProduct andEdge
+                 ((toListOf $ G.iedgesTo i . withIndex) g)
+                 ((toListOf $ G.iedgesFrom i . withIndex) g)
+
+  removeVertex i g = foldr ($) (G.delIdx i g) $ newEdges i g
+
+  reducible = filter (`elem` irreducible graph) $ G.idxs graph
