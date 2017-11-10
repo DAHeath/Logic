1813ce5aafb85b2e8c2add625675a0b7b39ca07f
Michael Eden <themichaeleden@gmail.com>
2017-11-10 09:08:47 -0500

    Added builtin method list and Java lib.

---
+++ java-parse/Wunderhorn.java
@@ -0,0 +1,8 @@
+public class Wunderhorn {
+  public static int arby_int() { return 0; }
+  public static boolean arby_bool() { return true; }
+  public static double arby_double() { return 0; }
+  public static float arby_float() { return 0; }
+  public static void ensure(boolean b) {}
+  public static void assume(boolean b) {}
+}
