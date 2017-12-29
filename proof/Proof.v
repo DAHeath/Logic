aa45a62697aa875de1baad5ee3c6a755ab65f8c9
David Heath <heath.davidanthony@gmail.com>
2017-12-29 15:35:08 -0500

    updated proof to include equivalence structure

--- proof/Proof.v
+++ proof/Proof.v
@@ -2,7 +2,6 @@ Require Import Coq.Init.Datatypes.
 Require Import Coq.Strings.String.
 Require Import Coq.Lists.List.
 Require Import Background.
-Require Import Maps.
 
 Inductive com : Type :=
   | CAss : id -> aexp -> com
@@ -68,6 +67,18 @@ Definition proc_named (ctx : context) (name : id) :=
 Definition change_body (cs : list com) (ctx : context) : context :=
   match ctx with
   | Context ct _ _ _ => context_at ct cs 0
+  end.
+
+Definition proc_body p : list com :=
+  match p with
+  | Proc _ _ cs => cs
+  end.
+
+Definition call (p : id) (ctx : context) : context :=
+  match ctx with
+  | Context ct p' _ _ =>
+      let cs := proc_body (ct p) in
+      context_at ct cs 0
   end.
 
 Reserved Notation "ps '/' st '\\' st'" (at level 40, st at level 39).
@@ -169,6 +180,8 @@ Proof.
   act E_Done.
 Qed.
 
+Module Single.
+
 Definition Assertion := state -> Prop.
 
 Definition assert_implies (P Q : Assertion) : Prop :=
@@ -191,7 +204,228 @@ Definition assn_sub X a P : Assertion :=
 Notation "P [ X |-> a ]" := (assn_sub X a P) (at level 10).
 Open Scope hoare_spec_scope.
 
-Theorem hoare_asgn : forall Q x a ctx,
+Definition bassn b : Assertion :=
+  fun st => (beval st b = true).
+
+Theorem hoare_asgn : forall P Q x a ctx,
   focus ctx = (x ::= a) ->
-  {{Q [X |-> a]}} ctx {{Q}}.
+  {{P}} next ctx {{Q}} ->
+  {{P [x |-> a]}} ctx {{Q}}.
 Proof.
+  unfold hoare_triple, assn_sub.
+  intros.
+  inversion H1; subst; try congruence.
+  rewrite H3 in H; inversion H; subst; clear H.
+  apply H0 in H5.
+  assumption.
+  assumption.
+Qed.
+
+Theorem hoare_jump : forall P Q ctx b i,
+  focus ctx = (b JUMP i) ->
+  {{fun st => P st /\ bassn b st}} move_context i ctx {{Q}} ->
+  {{fun st => P st /\ ~(bassn b st)}} next ctx {{Q}} ->
+  {{P}} ctx {{Q}}.
+Proof.
+  unfold hoare_triple, assn_sub, bassn.
+  intros.
+  inversion H2; subst; try congruence.
+  - rewrite H4 in H; inversion H; subst; clear H.
+    apply H0 in H6.
+    assumption.
+    split; assumption.
+  - rewrite H4 in H; inversion H; subst; clear H.
+    apply H1 in H6.
+    assumption.
+    split.
+    + assumption.
+    + rewrite Bool.not_true_iff_false.
+      assumption.
+Qed.
+
+Theorem hoare_done : forall P ctx,
+  focus ctx = DONE ->
+  {{P}} ctx {{P}}.
+Proof.
+  unfold hoare_triple, assn_sub.
+  intros.
+  inversion H0; congruence.
+Qed.
+
+Theorem hoare_invoke : forall P Q ctx outs p args,
+  focus ctx = (outs <== p CALL args) ->
+  ({{P}} ctx {{Q}} -> {{P}} call p ctx {{Q}}) ->
+  {{P}} ctx {{Q}}.
+Admitted.
+
+Theorem hoare_consequence_pre : forall (P P' Q : Assertion) c,
+  {{P'}} c {{Q}} ->
+  P ->> P' ->
+  {{P}} c {{Q}}.
+Proof.
+  intros P P' Q c Hhoare Himp.
+  intros st st' Hc HP. apply (Hhoare st st').
+  assumption. apply Himp. assumption.
+Qed.
+
+Theorem hoare_consequence_post : forall (P Q Q' : Assertion) c,
+  {{P}} c {{Q'}} ->
+  Q' ->> Q ->
+  {{P}} c {{Q}}.
+Proof.
+  intros P Q Q' c Hhoare Himp.
+  intros st st' Hc HP.
+  apply Himp.
+  apply (Hhoare st st').
+  assumption. assumption.
+Qed.
+
+End Single.
+
+Module Equiv.
+
+Definition Assertion := state -> state -> Prop.
+
+Definition triple
+           (P:Assertion) (c0 c1:context) (Q:Assertion) : Prop :=
+  forall st0 st0' st1 st1'
+  ,  c0 / st0 \\ st0'
+  -> c1 / st1 \\ st1'
+  -> P st0 st1 -> Q st0' st1'.
+
+Notation "{{ P }}  c0 , c1  {{ Q }}" :=
+  (triple P c0 c1 Q) (at level 90, c0, c1 at next level).
+
+Definition assert_implies (P Q : Assertion) : Prop :=
+  forall st0 st1, P st0 st1 -> Q st0 st1.
+
+Notation "P ->> Q" := (assert_implies P Q)
+                      (at level 80).
+
+Definition assn_sub_l X a P : Assertion :=
+  fun (st0 st1 : state) =>
+    P (t_update st0 X (aeval st0 a)) st1.
+Definition assn_sub_r X a P : Assertion :=
+  fun (st0 st1 : state) =>
+    P st0 (t_update st1 X (aeval st1 a)).
+Notation "P [[ X |-> a ]" := (assn_sub_l X a P) (at level 10).
+Notation "P [ X |-> a ]]" := (assn_sub_r X a P) (at level 10).
+
+Definition bassn_l b : Assertion :=
+  fun st0 st1 => (beval st0 b = true).
+
+Definition bassn_r b : Assertion :=
+  fun st0 st1 => (beval st1 b = true).
+
+Theorem asgn_l : forall P Q x a c0 c1,
+  focus c0 = (x ::= a) ->
+  {{P}} next c0, c1 {{Q}} ->
+  {{P [[x |-> a]}} c0, c1 {{Q}}.
+Proof.
+  unfold triple, assn_sub_l.
+  intros.
+  inversion H1; subst; try congruence.
+  rewrite H4 in H; inversion H; subst; clear H.
+  eapply H0 in H6.
+  - apply H6.
+  - apply H2.
+  - apply H3.
+Qed.
+
+Theorem asgn_r : forall P Q x a c0 c1,
+  focus c1 = (x ::= a) ->
+  {{P}} c0, next c1 {{Q}} ->
+  {{P [x |-> a]]}} c0, c1 {{Q}}.
+Proof.
+  unfold triple, assn_sub_l.
+  intros.
+  inversion H2; subst; try congruence.
+  rewrite H4 in H; inversion H; subst; clear H.
+  eapply H0 in H6.
+  - apply H6.
+  - apply H1.
+  - apply H3.
+Qed.
+
+Theorem jump_l : forall P Q c0 c1 b i,
+  focus c0 = (b JUMP i) ->
+  {{fun st0 st1 => P st0 st1 /\ bassn_l b st0 st1}} move_context i c0, c1 {{Q}} ->
+  {{fun st0 st1 => P st0 st1 /\ ~(bassn_l b st0 st1)}} next c0, c1 {{Q}} ->
+  {{P}} c0, c1 {{Q}}.
+Proof.
+  unfold triple, bassn_l.
+  intros.
+  inversion H2; subst; try congruence.
+  - rewrite H5 in H; inversion H; subst; clear H.
+    eapply H0 in H7.
+    + apply H7.
+    + apply H3.
+    + split; assumption.
+  - rewrite H5 in H; inversion H; subst; clear H.
+    eapply H1 in H7.
+    + apply H7.
+    + apply H3.
+    + split.
+      * assumption.
+      * rewrite Bool.not_true_iff_false.
+        assumption.
+Qed.
+
+Theorem jump_r : forall P Q c0 c1 b i,
+  focus c1 = (b JUMP i) ->
+  {{fun st0 st1 => P st0 st1 /\ bassn_r b st0 st1}} c0, move_context i c1 {{Q}} ->
+  {{fun st0 st1 => P st0 st1 /\ ~(bassn_r b st0 st1)}} c0, next c1 {{Q}} ->
+  {{P}} c0, c1 {{Q}}.
+Proof.
+  unfold triple, bassn_r.
+  intros.
+  inversion H3; subst; try congruence.
+  - rewrite H5 in H; inversion H; subst; clear H.
+    eapply H0 in H7.
+    + apply H7.
+    + apply H2.
+    + split; assumption.
+  - rewrite H5 in H; inversion H; subst; clear H.
+    eapply H1 in H7.
+    + apply H7.
+    + apply H2.
+    + split.
+      * assumption.
+      * rewrite Bool.not_true_iff_false.
+        assumption.
+Qed.
+
+Theorem done : forall P c0 c1,
+  focus c0 = DONE ->
+  focus c1 = DONE ->
+  {{P}} c0, c1 {{P}}.
+Proof.
+  unfold triple.
+  intros.
+  inversion H1; try congruence.
+  inversion H2; try congruence.
+Qed.
+
+Theorem consequence_pre : forall (P P' Q : Assertion) c0 c1,
+  {{P'}} c0, c1 {{Q}} ->
+  P ->> P' ->
+  {{P}} c0, c1 {{Q}}.
+Proof.
+  unfold triple, assert_implies.
+  intros.
+  apply (H st0 st0' st1 st1'); try assumption.
+  apply H0. assumption.
+Qed.
+
+Theorem consequence_post : forall (P Q Q' : Assertion) c0 c1,
+  {{P}} c0, c1 {{Q'}} ->
+  Q' ->> Q ->
+  {{P}} c0, c1 {{Q}}.
+Proof.
+  unfold triple, assert_implies.
+  intros.
+  apply H0.
+  apply (H st0 st0' st1 st1'); try assumption.
+Qed.
+
+End Equiv.
